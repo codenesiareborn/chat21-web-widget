c162a3d06f2bb2aaa6942b35f340cf98a999a471
Almafazi <almafazi@icloud.com>
2024-06-26 15:05:26 +0700

    fix

--- src/app/utils/utils-resources.ts
+++ src/app/utils/utils-resources.ts
@@ -1,13 +1,13 @@
 export const BRAND_BASE_INFO: { [key: string] : string} ={
-    COMPANY_NAME: "Tiledesk",
-    BRAND_NAME: "Tiledesk",
-    COMPANY_SITE_NAME:"tiledesk.com",
-    COMPANY_SITE_URL:"https://www.tiledesk.com",
+    COMPANY_NAME: "Dit SMP",
+    BRAND_NAME: "Dit SMP",
+    COMPANY_SITE_NAME:"ditsmp.kemdikbud.go.id",
+    COMPANY_SITE_URL:"https://ditsmp.kemdikbud.go.id",
     CONTACT_US_EMAIL: "support@tiledesk.com",
     FAVICON: "https://tiledesk.com/wp-content/uploads/2022/07/tiledesk_v13-300x300.png",
     META_TITLE:"Design Studio",
     LOGO_CHAT:"https://widget.tiledesk.com/v2/assets/images/tiledesk_logo_white_small.svg",
-    POWERED_BY:"<a tabindex='-1' target='_blank' ref='https://www.tiledesk.com/?utm_source=widget'><img src='https://support-pre.tiledesk.com/dashboard/assets/img/logos/tiledesk-solo_logo_new_gray.svg'/><span>Powered by Tiledesk</span></a>",
+    POWERED_BY:"<a tabindex='-1' target='_blank' ref='https://www.tiledesk.com/?utm_source=widget'><img src='https://support-pre.tiledesk.com/dashboard/assets/img/logos/tiledesk-solo_logo_new_gray.svg'/><span>Dit SMP Chat</span></a>",
 }
 
 export const LOGOS_ITEMS: { [key: string] : { label: string, icon: string }} ={
